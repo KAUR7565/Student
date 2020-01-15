46941f65826c1c16610ee25c594c7b661ee1f06b
KAUR7565 <kaur7565@sheridan.desire2learn.com>
2020-01-15 11:11:45 -0500

    2nd commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -48,6 +48,20 @@ public class Student {
         return email;
     }
 
+    /**
+     * @return the grades
+     */
+    public int getGrades() {
+        return grades;
+    }
+
+    /**
+     * @param grades the grades to set
+     */
+    public void setGrades(int grades) {
+        this.grades = grades;
+    }
+
 }
  
    
